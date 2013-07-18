328d6256b2e19c1562023a7f02a67bf1b891f884
glopez <gelopez91@outlook.com>
2013-07-18 10:56:16 -0600

    Cambie el objeto. Ahora es Movie.

---
+++ src/main/java/com/sdc/controller/Movie.java
@@ -0,0 +1,59 @@
+package com.sdc.controller;
+
+public class Movie {
+
+    String name;
+    String description;
+    String direction;
+    String year;
+    String language;
+    String duration;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String newDescription){
+        name = newDescription;
+    }
+
+    public String getDirection(){
+        return direction;
+    }
+
+    public void setDirection(String newDirection){
+        name = newDirection;
+    }
+
+    public String getYear(){
+        return year;
+    }
+
+    public void setYear(String newYear){
+        year = newYear;
+    }
+
+    public String getLanguage(){
+        return language;
+    }
+
+    public void setLanguage(String newLanguage){
+        language = newLanguage;
+    }
+
+    public String getDuration(){
+        return duration;
+    }
+
+    public void setDuration(String newDuration){
+        duration = newDuration;
+    }
+}
