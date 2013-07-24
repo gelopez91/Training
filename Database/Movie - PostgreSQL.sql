21874df7863f7fba09a7c7fea7a305dbf14c2efb
glopez <gelopez91@outlook.com>
2013-07-24 10:57:44 -0600

    Relacionados los actores con las peliculas

---
+++ Database/Movie - PostgreSQL.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE TABLE movie
+(
+	movie_id serial primary key,
+        name VARCHAR(40) not null,
+        description VARCHAR(100),
+        direction VARCHAR(40),
+        year VARCHAR(40) not null,
+        language VARCHAR(40) not null,
+        duration VARCHAR(40) not null
+);
