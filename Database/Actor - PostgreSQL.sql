fc4f4677b9bd2282d742002fc0b76f25c674d55d
glopez <gelopez91@outlook.com>
2013-07-24 10:55:58 -0600

    Relacionados los actores con las peliculas

---
+++ Database/Actor - PostgreSQL.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE TABLE actor
+(
+  actor_id serial NOT NULL,
+  firstName character varying(50) NOT NULL,
+  lastName character varying(50),
+  movie_id integer NOT NULL,
+  CONSTRAINT actor_pkey PRIMARY KEY (actor_id),
+  CONSTRAINT FK_movie FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
